a7bc296f52bd8d2b0217690b3cecae2ee349c528
Rajeev Santhosh <rajeev@lotuswireless.com>
2018-04-09 12:36:18 +0530

    update error send in multer

--- node-training/multer.js
+++ node-training/multer.js
@@ -7,34 +7,43 @@ var fs = require("fs");
 
 var cors = require('cors');
 
-file.post('/', multer().single('file'), function (req, res) {
+file.post('/', multer().single('file'), function (req, res, next) {
     // req.file is the `avatar` file 
     // req.body will hold the text fields, if there were any 
     debugger;
-    if (req.file) {
-        // console.dir(req.file);
-        // console.log(req.body);
-        var data = global.js_yyyy_mm_dd_hh_mm_ss();
-        var path = require('path');
-        var appDir = path.dirname(require.main.filename);
-        fs.writeFile(appDir + "/files/" + data + req.file.originalname, req.file.buffer, (err) => {
-            if (err) {
-                console.log('The file has been saved!');
-                return res.sendStatus(500).send({
-                    message: "Failed to store file",
-                    error: err
-                });
-            } else {
-                return res.send(req.file);
-            }
-        });
-        // upload.single("test")
-        // (req, res, function (err) {
-        //     console.log("File uploaded..");
-        //     res.end(req.body)
-        // })
-        // res.send(req.body)
+    try {
 
+        if (req.file) {
+            // console.dir(req.file);
+            // console.log(req.body);
+            var data = global.js_yyyy_mm_dd_hh_mm_ss();
+            var path = require('path');
+            var appDir = path.dirname(require.main.filename);
+            fs.writeFile(appDir + "/files/" + data + req.file.originalname, req.file.buffer, (err) => {
+                if (err) {
+                    console.log('The file has been saved!');
+                    return res.sendStatus(500).send({
+                        message: "Failed to store file",
+                        error: err
+                    });
+                } else {
+                    return res.send(req.file);
+                }
+            });
+            // upload.single("test")
+            // (req, res, function (err) {
+            //     console.log("File uploaded..");
+            //     res.end(req.body)
+            // })
+            // res.send(req.body)
+
+        }
+    } catch (error) {
+        var err = {
+            "status": 400,
+            message: error
+        };
+        return next(err);
     }
 })
 
